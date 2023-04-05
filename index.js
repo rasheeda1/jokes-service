9cd931e3aea3dadee3c45a2e3caf88477a632bb8
rasheedarahimi <rasheedarahimi10@outlook.com>
2023-04-05 15:28:36 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,87 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
+const { Joke, Sequelize } = require('./db');
+const { Op } = require("sequelize");
+const e = require('express');
 
+app.set("json spaces", "\t");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
+  if (req.query.tags){
+    try {
+      // TODO - filter the jokes by tags and content
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]: req.query.tags,
+          }
+        }
+      });
+      res.send(jokes);
+    } catch (error) {
+      console.error(error);
+      next(error)
+    }
+    return;
+  }
+  if (req.query.content){
+    try {
+      // TODO - filter the jokes by tags and content
+      const jokes = await Joke.findAll({
+        where: {
+          joke: {
+            [Op.substring]: req.query.content,
+          }
+        }
+      });
+      res.send(jokes);
+    } catch (error) {
+      console.error(error);
+      next(error)
+    }
+    return;
+  }
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const jokes = await Joke.findAll();
     res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error)
   }
 });
+app.get('/jokes/:id', async (req, res, next)=> {
+  try {
+    const jokes = await Joke.findByPk(req.params.id);
+    res.send(jokes)
+  } catch (error) {
+    console.error(error);
+    next(error)
+  }
+})
+app.post('/jokes', async(req, res, next) => {
+  try {
+    const joke = await Joke.create(req.body);
+    res.status(201).send(joke)
+  } catch (error) {
+    console.error(error);
+    next(error)
+    
+  }
+});
+
+app.put('/jokes/:id', async(req, res, next)=> {
+  try {
+    const joke = await Joke.put(req.body);
+    res.status(201).send(joke);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+})
+
+
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
